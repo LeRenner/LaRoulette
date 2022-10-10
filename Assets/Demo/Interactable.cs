e6dd8aff6e3b1008f1d90ae16e8adcce1235a2cf
JohnnyLima67 <64144848+JohnnyLima67@users.noreply.github.com>
2022-10-10 16:04:58 -0300

    Add Lobby

---
+++ Assets/Demo/Interactable.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public enum ButtonState : byte
+{
+    Open, Closed
+}
+
+public class Interactable : NetworkBehaviour
+{
+    private bool clicked = false;
+    [SyncVar]
+    public ButtonState buttonState;
+    [SyncVar]
+    public int readys = 0;
+
+
+    [Client]
+    void OnMouseUpAsButton()
+    {
+	Debug.Log(clicked);
+	clicked = !clicked;
+        CmdSetButtonState(clicked);
+
+
+    }
+
+    [Command(requiresAuthority = false)]
+    public void CmdSetButtonState(bool click,NetworkConnectionToClient sender = null)
+    {
+
+
+        if (buttonState == ButtonState.Open)
+        {
+	    buttonState = ButtonState.Closed;
+        }
+
+        if (buttonState == ButtonState.Closed)
+            buttonState = ButtonState.Open;
+
+	if (click)
+		readys += 1;
+	else
+		readys -= 1;
+	CmdStartMatch();
+
+
+    }
+
+    public void CmdStartMatch()
+    {
+      if (NetworkServer.connections.Count == readys)
+      {
+        NetworkManager.singleton.ServerChangeScene("minigame1");
+      }
+      return;
+    }
+
+}
